f52ad5c68440b5229454bee4c55af40d0f15b139
Marc Baloup <marc.baloup@etudiant.univ-lille1.fr>
2016-09-29 21:40:44 +0200

    Tab dans le pom + la classe 'main' est configuré dans le POM + retrait d'un warning dans le code

--- src/main/java/fr/univ_lille1/fil/pjea/TestJava8etu.java
+++ src/main/java/fr/univ_lille1/fil/pjea/TestJava8etu.java
@@ -7,7 +7,7 @@ public class TestJava8etu {
     public static void main(String[] args) throws Exception {
         ANTLRInputStream in = new ANTLRFileStream("src/main/java/fr/univ_lille1/fil/pjea/TestJava8etu.java");
         Java8Lexer lexer = new Java8Lexer(in);
-        Vocabulary voc = lexer.getVocabulary();
+        // Vocabulary voc = lexer.getVocabulary();
         for (Token tok : lexer.getAllTokens()) {
            System.out.println(tok.getText()); 
         }        
