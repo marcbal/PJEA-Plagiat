4ccf711bd146e77ac023fef5280ea921198bde01
Oudjail Veis <veis.oudjail@etudiant.univ-lille1.fr>
2016-10-04 11:25:29 +0200

    Question 7 et 8 fait

--- src/main/java/fr/univ_lille1/fil/pjea/TestJava8etu.java
+++ src/main/java/fr/univ_lille1/fil/pjea/TestJava8etu.java
@@ -2,14 +2,21 @@ package fr.univ_lille1.fil.pjea;
 import fr.univ_lille1.fil.pjea.analyzer.java8.*;
 import org.antlr.v4.runtime.*;
 
+
+
+
 public class TestJava8etu {
 
     public static void main(String[] args) throws Exception {
-        ANTLRInputStream in = new ANTLRFileStream("src/main/java/fr/univ_lille1/fil/pjea/TestJava8etu.java");
+    	if (args.length == 0)
+    		throw new IllegalArgumentException("Au moins un paramètre est requis");
+        ANTLRInputStream in = new ANTLRFileStream(args[0]);
         Java8Lexer lexer = new Java8Lexer(in);
         // Vocabulary voc = lexer.getVocabulary();
         for (Token tok : lexer.getAllTokens()) {
-           System.out.println(tok.getText()); 
+           System.out.println(tok.getText()+"	"+lexer.getVocabulary().getSymbolicName(tok.getType())); 
         }        
+        
+        
     }
 }
