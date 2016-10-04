6d9164bd372c9ae87f55f65d9c81d28692590758
zeroCrowsky <veis.oudjail@gmail.com>
2016-10-04 23:09:34 +0200

    Q9 fait, affichage de l'arbre des lexèmes

--- src/main/java/fr/univ_lille1/fil/pjea/TestJava8etu.java
+++ src/main/java/fr/univ_lille1/fil/pjea/TestJava8etu.java
@@ -1,6 +1,7 @@
 package fr.univ_lille1.fil.pjea;
 import fr.univ_lille1.fil.pjea.analyzer.java8.*;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 
 
@@ -12,10 +13,11 @@ public class TestJava8etu {
     		throw new IllegalArgumentException("Au moins un paramètre est requis");
         ANTLRInputStream in = new ANTLRFileStream(args[0]);
         Java8Lexer lexer = new Java8Lexer(in);
-        // Vocabulary voc = lexer.getVocabulary();
-        for (Token tok : lexer.getAllTokens()) {
-           System.out.println(tok.getText()+"	"+lexer.getVocabulary().getSymbolicName(tok.getType())); 
-        }        
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // create a parser that feeds off the tokens buffer
+        Java8Parser parser = new Java8Parser(tokens);
+        ParseTree tree = parser.compilationUnit(); // begin parsing at init rule
+        System.out.println(tree.toStringTree(parser).replaceAll("\\)", ")\n")); // print LISP-style tree      
         
         
     }
