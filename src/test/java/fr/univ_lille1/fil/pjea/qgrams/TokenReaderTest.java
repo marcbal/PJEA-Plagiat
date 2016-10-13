9b1f7abbd3b4114c6be32a51b4256e429dff416e
Badiane Pape-ibrahima <pi.badiane@etudiant.univ-lille1.fr>
2016-10-13 16:52:15 +0200

    test TokenReader

--- src/test/java/fr/univ_lille1/fil/pjea/qgrams/TokenReaderTest.java
+++ src/test/java/fr/univ_lille1/fil/pjea/qgrams/TokenReaderTest.java
@@ -2,6 +2,7 @@ package fr.univ_lille1.fil.pjea.qgrams;
 
 import static org.junit.Assert.*;
 
+
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.Lexer;
@@ -11,48 +12,35 @@ import org.junit.Test;
 
 import fr.univ_lille1.fil.pjea.antlr_lexers.java8.Java8Lexer;
 
-public class QGramTest {
+public class TokenReaderTest {
 
-	QGram qGram;
-	QGram qGramEqual;
-	
+		TokenReader toks;
 	@Before
 	public void setUp() throws Exception {
+		
 		ANTLRInputStream in = new ANTLRFileStream("src/test/resources/file1.java");
         Lexer l = new Java8Lexer(in);
-		qGram = new QGram(l.getAllTokens().subList(15, 25), 15, 134454645);
-		qGramEqual = new QGram(l.getAllTokens().subList(15, 25), 15, 134454645);
+		toks = new TokenReader(l.getAllTokens().subList(15, 25), 1, 1);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		qGram = null;
-		qGramEqual =null;
+		toks = null;
 	}
 
 	@Test
-	public void testHashCode() {
-		assertEquals(134454645,qGram.hashCode());
-		
+	public void testIterator() {
 	}
-
-	@Test
-	public void testGetQGramPosition() {
-		assertEquals(15,qGram.getQGramPosition());
-	}
-
 	
+	@Test
+	public void testEqualsTokens() {
+	}
 
 	@Test
-	public void testSize() {
-		assertEquals(10,qGram.size());
-		
+	public void testHashCodeToken() {
 	}
 	
 	
-	@Test
-	public void testAlignmentNeedlemanWunsch() {
-		assertEquals(qGram.size()*2, (qGramEqual.alignmentNeedlemanWunsch(qGram)));
-	}
 
 }
+
