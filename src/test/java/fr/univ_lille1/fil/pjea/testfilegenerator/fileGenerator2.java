2d800aa45d3d2e333308d5dd596512da1794048f
pacobadiane17 <pacobadiane17@gmail.com>
2016-11-09 22:13:27 +0100

    Concatenation fichier : public et package ignoré

--- src/test/java/fr/univ_lille1/fil/pjea/testfilegenerator/fileGenerator2.java
+++ src/test/java/fr/univ_lille1/fil/pjea/testfilegenerator/fileGenerator2.java
@@ -4,12 +4,12 @@ import java.io.*;
 class fileGenerator2{
 
 	static String  line = null;
-        public static void main(String[] args) {
+   public static void main(String[] args) {
 
  	try{
 
 		BufferedReader file = new BufferedReader(new FileReader("progCreationFichier.java"));
-		//BufferedReader file = new BufferedReader(new FileReader("f"));
+		BufferedReader file2 = new BufferedReader(new FileReader("PGMImage.java"));
 
 		FileOutputStream newFile = new FileOutputStream("newfichier.java");
 		BufferedWriter bwFile  = new BufferedWriter(new OutputStreamWriter(newFile));
@@ -29,7 +29,23 @@ class fileGenerator2{
 		
 	  }
 
+	  while((line=file2.readLine())!= null){
+		if(line.contains("package"))
+		   continue;
+	       if(line.contains("public")){
+		 line = line.replace("public","");
+	       }
+
+		for(int i=0 ; i<line.length();i++){
+			bwFile.write(line.charAt(i));	
+		}
+		bwFile.newLine();
+
+		
+	  }
+
 	  file.close();
+	  file2.close();
 	  bwFile.close();
 
 	}catch(Exception e){
@@ -39,6 +55,4 @@ class fileGenerator2{
       
   }
 
-
-	
 }
