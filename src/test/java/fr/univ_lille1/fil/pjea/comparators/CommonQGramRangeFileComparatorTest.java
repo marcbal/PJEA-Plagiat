f21586b2e769ab86a983200edcd08113af4cd9cf
Marc Baloup <marc.baloup@etudiant.univ-lille1.fr>
2016-11-10 01:30:25 +0100

    Ajout de tests unitaire pour CommonQGramRangeFileComparator

---
+++ src/test/java/fr/univ_lille1/fil/pjea/comparators/CommonQGramRangeFileComparatorTest.java
@@ -0,0 +1,23 @@
+package fr.univ_lille1.fil.pjea.comparators;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fr.univ_lille1.fil.pjea.Java8File;
+import fr.univ_lille1.fil.pjea.PlagiatVEMPTest;
+
+public class CommonQGramRangeFileComparatorTest {
+
+	@SuppressWarnings("static-method")
+	@Test
+	public void testComputeDifference() throws Exception {
+		double res = new CommonQGramRangeFileComparator(
+				new Java8File(PlagiatVEMPTest.TEST_PACK_1[0]), // file1.java
+				new Java8File(PlagiatVEMPTest.TEST_PACK_1[1]), // file1copy.java
+				5, 10)
+				.computeDifference();
+		assertEquals(1, res, 0.01);
+	}
+
+}
