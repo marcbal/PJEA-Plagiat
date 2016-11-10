a741c47b1ad24a6eecf17ef80016b0b1d7d36362
Marc Baloup <marc.baloup@etudiant.univ-lille1.fr>
2016-11-10 16:27:55 +0100

    Correction erreur de compilation

--- src/test/java/fr/univ_lille1/fil/pjea/testfilegenerator/FileGenerator.java
+++ src/test/java/fr/univ_lille1/fil/pjea/testfilegenerator/FileGenerator.java
@@ -7,7 +7,7 @@ import org.antlr.v4.runtime.Token;
 
 import fr.univ_lille1.fil.pjea.Java8File;
 
-public class fileGenerator {
+public class FileGenerator {
 
 	
 	public static void main(String[] args) {
