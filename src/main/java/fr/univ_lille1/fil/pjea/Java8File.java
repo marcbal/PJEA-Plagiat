4a28a96a2f4c067b822d7cd861527d28bed738be
Oudjail Veis <veis.oudjail@etudiant.univ-lille1.fr>
2016-11-10 16:24:45 +0100

    Ajout d'un nouvel attribut dans Java8File

--- src/main/java/fr/univ_lille1/fil/pjea/Java8File.java
+++ src/main/java/fr/univ_lille1/fil/pjea/Java8File.java
@@ -12,30 +12,32 @@ import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.Vocabulary;
 
 import fr.univ_lille1.fil.pjea.antlr_lexers.java8.Java8Lexer;
 
 public class Java8File {
 	
 	public final File file;
-	public final List<? extends Token> tokens;
+	public final List<Token> tokens;
 	public final List<String> fileLines;
+	public final Vocabulary vocabulary; 
 	
 	
 	public Java8File(String fileName) throws IOException {
 		file = new File(fileName);
-		tokens = Collections.unmodifiableList(getJava8Lexer(file).getAllTokens());
+		
+		Lexer lexer = getJava8Lexer(file);
+		
+		tokens = Collections.unmodifiableList(lexer.getAllTokens());
 		try (BufferedReader r = new BufferedReader(new FileReader(file))) {
 			fileLines = Collections.unmodifiableList(r.lines().collect(Collectors.toList()));
 		}
+		
+		vocabulary = lexer.getVocabulary();
 	}
 	
-	
-	
-	
-	
-	
-	
+
 	public static Lexer getJava8Lexer(File f) throws IOException {
         ANTLRInputStream in = new ANTLRFileStream(f.getPath());
         return new Java8Lexer(in);
