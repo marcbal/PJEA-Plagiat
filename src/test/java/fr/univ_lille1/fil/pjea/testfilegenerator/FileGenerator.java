ef32a5efa84ed9005c33cd35006aacd233b4103c
Oudjail Veis <veis.oudjail@etudiant.univ-lille1.fr>
2016-11-10 17:01:48 +0100

    Avancement du fusionneur de fichiers

--- src/test/java/fr/univ_lille1/fil/pjea/testfilegenerator/FileGenerator.java
+++ src/test/java/fr/univ_lille1/fil/pjea/testfilegenerator/FileGenerator.java
@@ -1,52 +1,90 @@
 package fr.univ_lille1.fil.pjea.testfilegenerator;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 import org.antlr.v4.runtime.Token;
 
 import fr.univ_lille1.fil.pjea.Java8File;
 
-public class fileGenerator {
+public class FileGenerator {
 
+	private static final String[] EOL_TOKENS = {";", "{", "}"};
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		Java8File[] files = Arrays.stream(args)
+				.map(arg -> {
+					try  {
+						return new Java8File(arg);
+					} catch (IOException e1) {
+						throw new UncheckedIOException(e1);
+					}
+				})
+				.toArray((length) -> new Java8File[length]);
 		
+		buildFile(files[0], files[1]);
 	}
 
 	
-	public static File BuildFile(Java8File f1, Java8File f2) throws Exception{
+	public static File buildFile(Java8File f1, Java8File f2) throws Exception {
 		
-		File newFile = new File("concact"+f1.file.getName()+"-"+f2.file.getName());
+		File newFile = new File("concact" + f1.file.getName() + "-" + f2.file.getName());
 		
-        Iterator<? extends Token> listToken1 = f1.tokens.iterator();
-        Iterator<? extends Token> listToken2 = f1.tokens.iterator();
+        List<Token> listToken1 = f1.tokens;
+        List<Token> listToken2 = f2.tokens;
         
-        //listToken1.remove();
-        //listToken2.remove();
-        
-        while(listToken1.hasNext()){
-  		  if(listToken1.next().equals("import")){
-  			  while(!listToken1.next().equals(";")){
-  				  
-  			  }
-  		  }
-  	  	}
+        List<Token> listTokenR = new ArrayList<>(listToken1);
+        listTokenR.addAll(listToken2);
         
         
-        while(listToken2.hasNext()){
-  		  if(listToken2.next().equals("import")){
-  			  
-  		  }
-  	  	}
+        List<Token> imports = new ArrayList<>();
+        List<Token> allDecl = new ArrayList<>();
+       
+        boolean nextClassDecl = false;
+        
+        Iterator<Token> iter = listTokenR.iterator();
+        for (Token token; iter.hasNext();) {
+        	token = iter.next();
+        	
+            /* Remove declaration package line */
+            if (token.getText() == "package") {
+    	        do {
+    	        	token = iter.next();
+    	        } while (!token.getText().equals(";")); 
+    	        	
+            } else if (token.getText() == "import") {
+        		do {
+        			imports.add(token);
+        			token = iter.next();
+        		} while (!token.getText().equals(";"));
+        			
+        	} else { /* other decl */  
+        		
+        	}
+        	
+        	
+        	//System.out.println(token.getText() + "	" + f1.vocabulary.getSymbolicName(token.getType()));
+        }
+        
+        /* Printing */ 
+        StringBuilder importsSb = new StringBuilder();
+        StringBuilder otherDeclSb = new StringBuilder();
+        //for ...
+        //if (Arrays.stream(EOL_TOKENS).anyMatch(e -> e.equals(token.getText()))) {
+    		importsSb.append('\n');
+    	//}
         
         
-
-
-		 
-		
-		 
-	
-		 return newFile;
+        
+        
+		return newFile;
 	}
+
+	
+	
 }
